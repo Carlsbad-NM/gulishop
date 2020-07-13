e4357b0c56b630d53b5397b1d0be3b85cdd59462
e <784248355@qq.com>
2020-07-13 09:13:31 +0800

    '3'

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,3 +3,22 @@ import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import Search from '@/pages/Search'
 
+export default [
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+]
+
